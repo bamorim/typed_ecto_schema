51dc1e08cf152062cc2290cbfaca0ad736528806
Bernardo Amorim <contato@bamorim.com>
2021-02-11 17:47:20 -0300

    prepare for v0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TypedEctoSchema.MixProject do
   def project do
     [
       app: :typed_ecto_schema,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
